9551dc17ec3eb026bed4565768bb81152b7e84e9
董枭 <dongx@2345.com>
2019-12-01 14:09:39 +0800

    提交测试代码

---
+++ src/Ohmgood/TestComposer/Tools.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ohmgood\TestComposer;
+
+class Tools
+{
+    /**
+     * echo "helloWorld";
+     */
+    public static function helloWorld()
+    {
+        echo "helloWorld";
+    }
+}
